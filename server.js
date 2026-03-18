54be81146b32daa9cd3201e46d8db2eda6b0a4e6
Adeyinka ADedayo <adeyinkaadedayo1024@mail.com>
2026-03-18 23:49:57 +0100

    Fix PORT for Render deployment

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 const mongodb = require('./db/connect');
 const swaggerUi = require('swagger-ui-express');
